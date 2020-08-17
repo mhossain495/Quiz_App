c9ef48374c02604def2f17017bba2cf722234c80
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-08-16 21:30:19 -0400

    Change to MVC design

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,70 @@
+//  ViewController.swift
+//  Quizzler-iOS13
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // Calling function to display question on UI.
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight == true {
+            sender.backgroundColor = UIColor.green.withAlphaComponent(0.5)
+        } else {
+            sender.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+        }
+        
+        // Go to next question in array
+        
+        quizBrain.nextQuestion()
+        
+        // Time delay between animations.
+        
+        let delay = 0.2
+        UIView.animate(withDuration: delay, animations: { self.updateUI()
+        })
+        
+    }
+    
+    // Function to update progress bar, change question, and clear True/False button colors after answering question.
+    
+    func updateUI() {
+        progressBar.progress = quizBrain.getProgress()
+        questionLabel.text = quizBrain.getQuestionText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+    }
+    
+    
+}
+
+
